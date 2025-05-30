cb7ab1b719d1e98c51ca78ad11b4050f5a205c10
Debottam Kar <debottamkar2003@gmail.com>
2025-05-31 00:02:40 +0530

    Day2

--- 02_Operators/operator.rs
+++ 02_Operators/operator.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, Rust!");
+    let num1: i32 = 5 + 10;
+    let num2: i32 = 5 * 10;
+    let num3: i32 = 5 - 10;
+    let num4: i32 = 5 / 10;
+    let num5: i32 = 5 % 2;
+    let name: &str = "Debottam Kar";
+    println!("{} {} {} {} {}", num1, num2, num3, num4, num5);
+    println!("{}", name);
 }
---
+++ 03_Conditionals/conditional.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number = 7;
+
+    if number < 10 {
+        println!("The number is less than 10.");
+    } else {
+        println!("The number is 10 or greater.");
+    }
+}
